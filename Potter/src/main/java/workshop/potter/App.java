6193eccf35f4742a64b2aa68a872ebfcac93dcb0
BNNProduction <uthai.boonyaba@gmail.com>
2021-02-09 09:59:16 +0700

    save

---
+++ Potter/src/main/java/workshop/potter/App.java
@@ -0,0 +1,13 @@
+package workshop.potter;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
